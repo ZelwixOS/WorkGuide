9b75c6e96f4bbf47b0e08ba944af5d6085fb6b44
Zelwix <vafin_01@mail.ru>
2023-02-18 13:06:24 +0300

    Backend initial commit

---
+++ WorkGuideBack/DAL/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿namespace DAL.Entities
+{
+    using Microsoft.AspNetCore.Identity;
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Diagnostics.CodeAnalysis;
+
+    public class User : IdentityUser<Guid>
+    {
+        [Required]
+        [MaxLength(100)]
+        [MinLength(1)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string SecondName { get; set; }
+
+        [MaxLength(100)]
+        [AllowNull]
+        public string? Avatar { get; set; }
+
+        public bool Banned { get; set; }
+    }
+}
---
+++ WorkGuideBack/WorkGuideBack/Program.cs
@@ -0,0 +1,47 @@
+using WorkGuideBack;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+AppConfigurator.Configure(builder);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+
+app.UseHttpsRedirection();
+
+app.UseSpaStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+});
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+});
+
+app.MapControllers();
+
+app.Run();
