d8e762fd50f262090e333756bef29cb4649c2b42
Sokolov Daniil <sokol_sokolov@list.ru>
2023-03-29 18:04:16 +0300

    Fix naming for test answers

--- WorkGuideBack/BLL/DTO/Request/Answer/TestAnswerRequestDto.cs
+++ WorkGuideBack/BLL/DTO/Request/Answer/TestAnswerRequestDto.cs
@@ -5,14 +5,14 @@ namespace BLL.DTO.Request.Answer
     public class TestAnswerRequestDto : IDtoMapper<DAL.Entities.Answer>
     {
         public string Content { get; set; }
-        public bool IsValide { get; set; }
+        public bool IsValid { get; set; }
 
         public DAL.Entities.Answer ToModel()
         {
             return new DAL.Entities.Answer()
             {
                 Content = this.Content,
-                IsValid = this.IsValide,
+                IsValid = this.IsValid,
             };
         }
     }
